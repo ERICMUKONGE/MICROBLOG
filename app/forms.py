f7f61f3793bb57865e5b6fe35464518f5250aa50
MUKONGE ERIC <mukonge.eric@gmail.com>
2022-08-22 02:13:25 -0700

    form

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import ValidationError,DataRequired,Email, EqualTo
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 from app.models import User
 
 class LoginForm(FlaskForm):
