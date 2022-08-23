50c72b72c31410fc87cd6e7a887f1910cfb94d24
MUKONGE ERIC <mukonge.eric@gmail.com>
2022-08-22 23:50:38 -0700

    Addition of avatar

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
+from hashlib import md5
 
 class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -10,12 +11,18 @@ class User(UserMixin,db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(404))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow) 
     
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self,size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?=identicon&s={}'.format(digest, size)    
 
     def __repr__(self):
         return "<User {}>".format(self.username)
--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,7 @@ from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user,logout_user,login_required
 from app.models import User
 from werkzeug.urls import url_parse 
+from datetime import datetime
 
 @app.route('/')
 @app.route('/index')
@@ -68,4 +69,10 @@ def user(username):
         {'author': user, 'body': 'Test post #1'},
         {'author': user, 'body': 'Test post #2'}
     ]
-    return render_template('user.html', user=user, posts=posts)     
+    return render_template('user.html', user=user, posts=posts)   
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.lstseen = datetime.utcnow()
+        db.session.commit()      
