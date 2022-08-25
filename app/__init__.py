729f4e421becf95fe638f8fb8d9ec94d7c59be9e
MUKONGE ERIC <mukonge.eric@gmail.com>
2022-08-24 21:08:29 -0700

    errors

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,10 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import  logging
+from logging.handlers import SMTPHandler
+from logging.handlers import RotatingFileHandler
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -20,4 +24,30 @@ def make_shell_context():
 def load_user(id):
     return User.query.get(int(id))
 
-from app import routes, models
+from app import routes, models, errors
+
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'],app.config['MALI_PORT']),
+            fromaddr=app.config['ADMINS'], subject='microblog Failure',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)  
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_hander = RotatingFileHandler('Logs/microblog.log', maxBytes=10240,backupCount=10)
+    file_hander.setFormatter(logging.Formatter('%(asctime)s %(lev elname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_hander.setLevel(logging.INFO)
+    app.logger.addHander(file_hander)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')      
+               
