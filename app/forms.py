fac454e096c13efc29c40ea944f01a321354c91c
MUKONGE ERIC <mukonge.eric@gmail.com>
2022-08-26 04:06:30 -0700

    Database relationship

--- app/forms.py
+++ app/forms.py
@@ -31,10 +31,10 @@ class EditProfileForm(FlaskForm):
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
 
-class EditProfileForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
-    submit = SubmitField('Submit')    
+# class EditProfileForm(FlaskForm):
+#     username = StringField('Username', validators=[DataRequired()])
+#     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+#     submit = SubmitField('Submit')    
 
     def __init__(self, original_username, *args, **kwargs):
         super(EditProfileForm, self).__init__(*args,**kwargs)
--- app/models.py
+++ app/models.py
@@ -5,6 +5,11 @@ from flask_login import UserMixin
 from app import login
 from hashlib import md5
 
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -32,10 +37,27 @@ class Post(db.Model):
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-     
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers',lazy='dynamic'), lazy='dynamic')   
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self .is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+                         
 
 
     
