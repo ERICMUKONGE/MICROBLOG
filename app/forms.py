a2e23444f1ba7a04365248464d1325050e98e4a8
MUKONGE ERIC <mukonge.eric@gmail.com>
2022-08-27 00:39:03 -0700

    update

--- app/forms.py
+++ app/forms.py
@@ -31,11 +31,6 @@ class EditProfileForm(FlaskForm):
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
 
-# class EditProfileForm(FlaskForm):
-#     username = StringField('Username', validators=[DataRequired()])
-#     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
-#     submit = SubmitField('Submit')    
-
     def __init__(self, original_username, *args, **kwargs):
         super(EditProfileForm, self).__init__(*args,**kwargs)
         self.original_username = original_username
@@ -45,3 +40,7 @@ class EditProfileForm(FlaskForm):
             user = User.query.filter_by(username=self.username.data).first()
             if user is not None:
                 raise ValidationError("Please use a different.")
+
+
+class EmptyForm(FlaskForm):
+    submit = SubmitField('submit')
--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,7 @@ from flask_login import current_user, login_user,logout_user,login_required
 from app.models import User
 from werkzeug.urls import url_parse 
 from datetime import datetime
+from app.forms import EmptyForm
 
 @app.route('/')
 @app.route('/index')
@@ -90,6 +91,47 @@ def edit_profile():
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
-    return render_template('edit_profile.html', title='Edit Profile', form=form)       
+    return render_template('edit_profile.html', title='Edit Profile', form=form) 
+
+@app.route('/follow/<username>', methods=['POST']) 
+@login_required
+def follow(username):
+    form = EmptyForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            flash('User {} not found.'.format(username))
+            return redirect(url_for('index'))
+        if user == current_user:
+            flash('You cannot follow yourself!')
+            return redirect(url_for('user', username=username))
+        current_user.follow(user)
+        db.session.commit()
+        flash('You are following {} !'.format(username))
+        return redirect(url_for('username', username=username))
+    else:
+        return redirect(url_for('index'))
+
+@app.route('/unfollow/<username>', methods=['POST'])
+@login_required
+def unfollow(username):
+    form = EmptyForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            flash('User {} not found.'.format(username))
+            return redirect(url_for('index'))
+        if user ==  current_user:
+            flash('You cannot unfollow yourself!')
+            return redirect(url_for('user', username=username))
+        current_user.follow(user)
+        db.session.commit()
+        flash('You are not following {}.'.format(username))
+        return redirect(url_for('user', username=username))
+    else:
+        return redirect(url_for('index'))     
+        
+                                 
+
 
 
