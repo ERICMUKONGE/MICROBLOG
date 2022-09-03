6a52172256fac7bf3b1082e4ce687921f1c404f3
MUKONGE ERIC <mukonge.eric@gmail.com>
2022-09-03 15:20:41 -0700

    facefit

--- app/email.py
+++ app/email.py
@@ -1,6 +1,7 @@
 from flask_mail import Message
 from app import mail, app
 from flask import render_template
+from threading import Thread
 
 def send_email(subject,sender,recipients,text_body,html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
